c4e9be7e74ea0528f2b904d061ff6ca1ec0f9887
RiccardoSansone <r.sansone@gmail.com>
2023-09-26 15:30:10 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+
+// import AppNavbar from './components/AppNavbar.vue';
+
+export default {
+  data() {
+    return {
+    }
+  },
+  components: {
+  }
+}
+</script>
+
+<template>
+
+</template>
+
+<style>
+
+</style>
